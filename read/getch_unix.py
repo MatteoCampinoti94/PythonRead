4c4faef1d221c54825b5528834ab9d7adc6e1fed
Matteo Campinoti <matteo.campinoti94@gmail.com>
2017-12-28 00:20:39 +0100

    Added custom encoding

--- read/getch_unix.py
+++ read/getch_unix.py
@@ -1,7 +1,7 @@
 import sys, tty, termios
 import fcntl, os
 
-def getch_unix(NONBLOCK=False):
+def getch_unix(NONBLOCK=False, encoding='utf-8'):
     fd = sys.stdin.fileno()
     settings = termios.tcgetattr(fd)
     flags = fcntl.fcntl(fd, fcntl.F_GETFL)
@@ -10,7 +10,7 @@ def getch_unix(NONBLOCK=False):
         tty.setraw(fd)
         if NONBLOCK:
             fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
-        ch = sys.stdin.read(1)
+        ch = sys.stdin.buffer.read(1).decode(encoding)
     finally:
         fcntl.fcntl(fd, fcntl.F_SETFL, flags)
         termios.tcsetattr(fd, termios.TCSADRAIN, settings)
