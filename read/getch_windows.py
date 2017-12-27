fde173072f5e9a44cdff3f6705624cf4bde3d409
Matteo Campinoti <matteo.campinoti94@gmail.com>
2017-12-28 00:06:20 +0100

    Added encoding option + fixes
    Encoding can be passed to the function
    If NONBLOCK is true and stdin is empty then return empty string

--- read/getch_windows.py
+++ read/getch_windows.py
@@ -1,8 +1,10 @@
 import msvcrt
 
-def getch_windows(NONBLOCK=False):
-    if NONBLOCK:
+def getch_windows(NONBLOCK=False, encoding='utf-8'):
+	if NONBLOCK:
 		if msvcrt.kbhit():
-			return msvcrt.getch().decode()
+			return msvcrt.getch().decode(encoding)
+		else:
+			return ''
 	else:
-		return msvcrt.getch().decode()
+		return msvcrt.getch().decode(encoding)
