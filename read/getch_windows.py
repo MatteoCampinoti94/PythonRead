207e1ebf958e06a5ac3a16c901b449bcb031b3e2
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2017-12-28 01:06:41 +0100

    Cleaned up

--- read/getch_windows.py
+++ read/getch_windows.py
@@ -1,10 +1,9 @@
 import msvcrt
 
 def getch_windows(NONBLOCK=False, encoding='utf-8'):
-	if NONBLOCK:
-		if msvcrt.kbhit():
-			return msvcrt.getch().decode(encoding)
-		else:
-			return ''
-	else:
+	if NONBLOCK and not msvcrt.kbhit():
+		return ''
+	if encoding:
 		return msvcrt.getch().decode(encoding)
+	else:
+		return msvcrt.getwch()
